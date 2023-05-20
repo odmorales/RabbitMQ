119f80af38197ca0eee8a90d52d253e36bf9b1ce
Your Name <you@example.com>
2023-05-20 17:12:16 -0500

    first commit

---
+++ MicroservicesRabbitMQ/MicroRabbit.Transfer.Api/Controllers/TransferController.cs
@@ -0,0 +1,23 @@
+﻿using MicroRabbit.Transfer.Domain.Interfaces;
+using MicroRabbit.Transfer.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroRabbit.Transfer.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransferController : ControllerBase
+    {
+        private readonly ITransferRepository _transferRepository;
+
+        public TransferController(ITransferRepository transferRepository)
+        {
+            _transferRepository = transferRepository;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<TransferLog>> Get()
+        {
+            return Ok(_transferRepository.GetTransferLogs());
+        }
+    }
+}
